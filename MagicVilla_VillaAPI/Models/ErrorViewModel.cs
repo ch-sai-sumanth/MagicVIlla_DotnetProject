7938621193aac5c6ba149a8da8d2e2e5ad3c544b
ch-sai-sumanth <saisumanth0531@gmail.com>
2024-04-25 17:17:05 +0530

    first commit

---
+++ MagicVilla_VillaAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MagicVilla_VillaAPI.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
